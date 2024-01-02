865be7804d00996d518f4d8c27e28cff6cf6bc19
Rafal Urbaniak <rafalur@gmail.com>
2024-01-02 22:42:55 +0100

    Initial Commit

---
+++ Sentences/SentencesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentencesApp.swift
+//  Sentences
+//
+//  Created by Rafal Urbaniak on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SentencesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
